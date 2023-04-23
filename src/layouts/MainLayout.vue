64219db0b441554f1e5b07a0dc856f12032ac1f3
Manuela da Cruz Chadreque <manuelachadreque@gmail.com>
2023-04-23 11:26:33 +0200

    innitial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,170 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+      </q-toolbar>
+      <div class ="q-qx-lg q-pt-x q-mb-md">
+        <div class="text-h3">Todo</div>
+        <div class="text-subtitle1">{{ todaysDate }}</div>
+      </div>
+      <q-img
+
+        src ="statics\frequency.jpg"
+        class="header-image absolute-top"
+      />
+
+    </q-header>
+
+    <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="240"
+        :breakpoint="600"
+      >
+        <q-scroll-area style="height: calc(100% - 144px); margin-top: 150px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item
+              to="/"
+              exact
+              clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+
+              <q-item-section>
+                Todo
+              </q-item-section>
+            </q-item>
+
+
+            <q-item
+            to="/help"
+            exact
+            clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="help" />
+              </q-item-section>
+
+              <q-item-section>
+                Help
+              </q-item-section>
+            </q-item>
+          </q-list>
+
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="statics\frequency.jpg" style="height: 144px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://0.gravatar.com/userimage/99506154/def2161308ae8a1c9e843ac167fa996e?size=original">
+            </q-avatar>
+            <div class="text-weight-bold">Manuela Chadreque</div>
+            <div>@mcchadreque</div>
+          </div>
+        </q-img>
+      </q-drawer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from 'quasar'
+
+import { computed, defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+
+  },
+  computed: {
+      todaysDate(){
+        let timeStamp = Date.now()
+        return date.formatDate(timeStamp, 'dddd D MMMM YYYY')
+      }
+    }
+
+})
+</script>
+
+<style lang ="scss">
+  .header-image{
+    height: 100%;
+    z-index: -1;
+    opacity: 0.2;
+    filter: grayscale(100%);
+
+  }
+
+</style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-la">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus harum esse exercitationem totam ut, possimus modi, quis natus cupiditate ipsam sunt atque quia quibusdam aut dolores vel illum ullam quae!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, alias? Ullam adipisci voluptates ab dolor repellendus voluptate nemo enim facilis fuga. Aut unde consequuntur possimus magnam sint omnis neque dolorem.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+})
+</script>
